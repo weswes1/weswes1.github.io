500daaf7822114c17bd0209f5a9d0fe93bcb15ef
Wesley <40642588+weswes1@users.noreply.github.com>
2019-11-09 00:14:56 +0900

    Update NEWGAME.js

--- NEWGAME.js
+++ NEWGAME.js
@@ -1,3 +1,11 @@
+
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
